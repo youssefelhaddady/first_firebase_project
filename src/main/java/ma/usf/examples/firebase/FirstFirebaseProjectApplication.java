f332b1fc91d2c13604d558613658544b95b43e80
YouSsef <youssef.elhaddady@usmba.ac.ma>
2020-11-25 23:34:39 +0100

    project creation

---
+++ src/main/java/ma/usf/examples/firebase/FirstFirebaseProjectApplication.java
@@ -0,0 +1,13 @@
+package ma.usf.examples.firebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstFirebaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstFirebaseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/usf/examples/firebase/FirstFirebaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.usf.examples.firebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstFirebaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
